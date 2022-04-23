03099f8cc693f667de4ee5a21b7600fa127ecfda
Stewart Million-Perez <stewart.millionperez@und.edu>
2022-04-23 10:51:44 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bug_Smasher_Tasker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
